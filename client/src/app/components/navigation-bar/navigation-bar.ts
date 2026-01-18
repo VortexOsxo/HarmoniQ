ba06e841afcad8e2fb570c7e7c58f1b979c132d6
Philippe Martin <philippeoliviermartin@gmail.com>
2026-01-18 14:04:08 -0500

    created angular app and added default pages

---
+++ client/src/app/components/navigation-bar/navigation-bar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-bar',
+  imports: [],
+  templateUrl: './navigation-bar.html',
+  styleUrl: './navigation-bar.css',
+  standalone: true,
+})
+export class NavigationBar {
+
+}
