0d2254b79560d346f2302d9e6bcd79329c3ab230
Philippe Martin <philippeoliviermartin@gmail.com>
2026-01-18 14:22:56 -0500

    added not found page

---
+++ client/src/app/pages/not-found-page/not-found-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavigationBar } from '@app/components/navigation-bar/navigation-bar';
+
+@Component({
+  selector: 'app-not-found-page',
+  imports: [NavigationBar],
+  templateUrl: './not-found-page.html',
+  styleUrl: './not-found-page.css'
+})
+export class NotFoundPage {
+
+}
