9714bb0a7a21e8c1d133070fb9ca2f07a4a392ef
Philippe Martin <philippeoliviermartin@gmail.com>
2026-01-18 14:22:56 -0500

    added not found page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { AboutPage } from './pages/about-page/about-page';
+import { NotFoundPage } from './pages/not-found-page/not-found-page';
 
 export const routes: Routes = [
     { path: 'about', component: AboutPage },
-    { path: '**', component: HomePage },
+    { path: 'home', component: HomePage },
+    { path: '', component: HomePage },
+    { path: 'not-found', component: NotFoundPage },
+    { path: '**', component: NotFoundPage },
 ];
