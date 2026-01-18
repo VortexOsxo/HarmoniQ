75279e9cfc5662f308ad40c81d7bd26c5eb25b35
Philippe Martin <philippeoliviermartin@gmail.com>
2026-01-18 14:47:28 -0500

    added docs page

--- client/src/app/components/navigation-bar/navigation-bar.ts
+++ client/src/app/components/navigation-bar/navigation-bar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navigation-bar.html',
   styleUrl: './navigation-bar.css',
   standalone: true,
