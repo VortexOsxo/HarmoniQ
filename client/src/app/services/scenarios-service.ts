16602b2f53ff35ffb2ac194005ce1fb727cee4a0
Philippe Martin <philippeoliviermartin@gmail.com>
2026-01-19 08:57:20 -0500

    improved selection responsivness when creating/deleting

--- client/src/app/services/scenarios-service.ts
+++ client/src/app/services/scenarios-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { getScenarioFromJson, scenarioToJson } from '@app/models/scenario';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { signal } from '@angular/core';
 import { Scenario } from '@app/models/scenario';
 
@@ -10,33 +10,45 @@ import { Scenario } from '@app/models/scenario';
   providedIn: 'root',
 })
 export class ScenariosService {
+  scenarios = signal<Scenario[]>([]);
   selectedScenario = signal<Scenario | null>(null);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.refreshScenarios().subscribe();
+  }
+
+  refreshScenarios() {
+    return this.http.get(environment.apiUrl + '/scenario/', { headers: { 'Content-Type': 'application/json' } })
+      .pipe(
+        map((data: any) => data.map((scenario: any) => getScenarioFromJson(scenario))),
+        tap((scenarios: Scenario[]) => this.scenarios.set(scenarios))
+      );
+  }
 
   fetchScenarios() {
-    return this.http.get(environment.apiUrl + '/scenario/', { headers: { 'Content-Type': 'application/json' } })
-      .pipe(map((data: any) => data.map((scenario: any) => getScenarioFromJson(scenario))));
+    return this.refreshScenarios();
   }
 
   createScenario(scenario: Scenario) {
     return this.http.post(environment.apiUrl + '/scenario/', scenarioToJson(scenario), { headers: { 'Content-Type': 'application/json' } })
-      .pipe(map((data: any) => getScenarioFromJson(data)));
+      .pipe(
+        map((data: any) => getScenarioFromJson(data)),
+        tap((newScenario) => {
+          this.scenarios.update(s => [...s, newScenario]);
+          this.selectedScenario.set(newScenario);
+        })
+      );
   }
 
   deleteScenario(scenario: Scenario) {
-
     return this.http.delete(environment.apiUrl + '/scenario/' + scenario.id, { headers: { 'Content-Type': 'application/json' } })
-      .pipe(map((data: any) => {
-        if (!data) {
-          return null;
-        }
-
-        if (scenario.id === this.selectedScenario()?.id) {
-          this.selectedScenario.set(null);
-        }
-
-        return getScenarioFromJson(data);
-      }));
+      .pipe(
+        tap(() => {
+          this.scenarios.update(s => s.filter(item => item.id !== scenario.id));
+          if (this.selectedScenario()?.id === scenario.id) {
+            this.selectedScenario.set(null);
+          }
+        })
+      );
   }
 }
