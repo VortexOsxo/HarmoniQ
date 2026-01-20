0f766b059ee7b600302b7e18c8ddac2af4c17780
Philippe Martin <philippeoliviermartin@gmail.com>
2026-01-19 19:08:16 -0500

    added accordeon with all infrastructures

---
+++ client/src/app/components/infrastructure/infrastructure-selector/infrastructure-selector.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { SolarFarmsService } from '@app/services/solar-farms-service';
+import { CommonModule } from '@angular/common';
+import { InfrastruturesService } from '@app/services/infrastrutures-service';
+import { InfrastructureGroup } from '@app/models/infrastructure-group';
+import { InfraListElement } from '../infra-list-element/infra-list-element';
+import { HydroelectricDamsService } from '@app/services/hydroelectric-dams-service';
+import { NuclearPowerPlantsService } from '@app/services/nuclear-power-plants-service';
+import { ThermalPowerPlantsService } from '@app/services/thermal-power-plants-service';
+import { WindFarmsService } from '@app/services/wind-farms-service';
+import { InfraListBody } from '../infra-list-body/infra-list-body';
+
+@Component({
+  selector: 'app-infrastructure-selector',
+  imports: [CommonModule, FormsModule, NgbAccordionModule, InfraListElement, InfraListBody],
+  templateUrl: './infrastructure-selector.html',
+  styleUrl: './infrastructure-selector.css',
+})
+export class InfrastructureSelector {
+
+  get infrastructureGroups(): InfrastructureGroup[] {
+    return this.infrastructuresService.infraGroups();
+  }
+
+  get selectedInfrastructureGroup(): InfrastructureGroup | null {
+    return this.infrastructuresService.selectedInfraGroup();
+  }
+
+  set selectedInfrastructureGroup(infrastructureGroup: InfrastructureGroup | null) {
+    this.infrastructuresService.selectedInfraGroup.set(infrastructureGroup);
+  }
+
+  infras: any[] = []
+
+  constructor(
+    private infrastructuresService: InfrastruturesService,
+    public solarFarmsService: SolarFarmsService,
+    public hydroelectricDamsService: HydroelectricDamsService,
+    public nuclearPowerPlantsService: NuclearPowerPlantsService,
+    public thermalPowerPlantsService: ThermalPowerPlantsService,
+    public windFarmsService: WindFarmsService
+  ) {
+    this.infras = [
+      { name: 'Barrage Hydro-Électrique', type: 'hydro', infras: this.hydroelectricDamsService.infras },
+      { name: 'Parc Éolien', type: 'eolienneparc', infras: this.windFarmsService.infras },
+      { name: 'Parc Solaire', type: 'solaire', infras: this.solarFarmsService.infras },
+      { name: 'Centrale Thermique', type: 'thermique', infras: this.thermalPowerPlantsService.infras },
+      { name: 'Centrale Nucléaire', type: 'nucleaire', infras: this.nuclearPowerPlantsService.infras }
+    ]
+  }
+
+  compareInfraGroups(g1: InfrastructureGroup, g2: InfrastructureGroup): boolean {
+    return g1 && g2 ? g1.id === g2.id : g1 === g2;
+  }
+
+}
--- client/src/app/pages/simulation-page/simulation-page.ts
+++ client/src/app/pages/simulation-page/simulation-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavigationBar } from '@app/components/navigation-bar/navigation-bar';
 import { SimulationLauncher } from '@app/components/simulation/simulation-launcher/simulation-launcher';
 import { ScenarioSelector } from '@app/components/scenario/scenario-selector/scenario-selector';
+import { InfrastructureSelector } from '@app/components/infrastructure/infrastructure-selector/infrastructure-selector';
 
 @Component({
   selector: 'app-simulation-page',
-  imports: [NavigationBar, SimulationLauncher, ScenarioSelector],
+  imports: [NavigationBar, SimulationLauncher, ScenarioSelector, InfrastructureSelector],
   templateUrl: './simulation-page.html',
   styleUrl: './simulation-page.css',
 })
