f6e25e3a14a1bcded7c1b324d123277a6be55f45
Philippe Martin <philippeoliviermartin@gmail.com>
2026-01-19 19:52:16 -0500

    added logic to modify/create infra group

--- client/src/app/components/infrastructure/infra-list-element/infra-list-element.ts
+++ client/src/app/components/infrastructure/infra-list-element/infra-list-element.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { InfrastruturesService } from '@app/services/infrastrutures-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-infra-list-element',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './infra-list-element.html',
 })
 export class InfraListElement {
@@ -10,9 +12,15 @@ export class InfraListElement {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) type!: string;
 
-  constructor() { }
+  get isSelected(): boolean {
+    return this.infrastructuresService.isInfraSelected(this.type, this.id);
+  }
 
-  add_infra() { }
+  constructor(private infrastructuresService: InfrastruturesService) { }
+
+  toggleInfra() {
+    this.infrastructuresService.toggleInfra(this.type, this.id);
+  }
 
   simulate_single() { }
 
