809de4036ea9feb456bcf30f07433e97baa1d0c9
PierreOlivierP <pierre-olivier.piquant@polymtl.ca>
2026-02-13 00:16:19 -0500

    modified game to be 10 question based, end game management not done

--- client/src/app/pages/game-page/game-page.ts
+++ client/src/app/pages/game-page/game-page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationBar } from "@app/components/navigation-bar/navigation-bar";
 import { GameArea } from "@app/components/game-area/game-area";
+import { MatDialog } from '@angular/material/dialog'; 
 
 @Component({
   selector: 'app-game-page',
@@ -9,5 +10,20 @@ import { GameArea } from "@app/components/game-area/game-area";
   styleUrl: './game-page.css',
 })
 export class GamePage {
+  constructor(private dialog: MatDialog){}
 
+
+  ngOnInit(){
+    this.openDialog();
+  }
+
+
+  openDialog(){
+    const dialogRef = this.dialog.open(GameArea, {
+      width: '60vw',
+      height: '60vh',
+      maxWidth: 'none',
+      panelClass: 'dialogPanel'
+    })
+  }
 }
