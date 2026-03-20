b4bcd7eab1b844a41e8fd649b1a220bdf776d6a6
Jérôme Fréchette <jerome.frechette@polymtl.ca>
2026-03-19 23:33:26 -0400

    db link

--- harmoniQ/harmoniq/scripts/load_database.py
+++ harmoniQ/harmoniq/scripts/load_database.py
@@ -7,8 +7,7 @@ GOOGLE_DRIVE_FILE_ID = "1EA596DXPYxUMKDWa3L9WyKiwzGBW_7aP"
 def download_db():
     print(f"Téléchargement de la base de données (ID: {GOOGLE_DRIVE_FILE_ID})...")
     try:
-        file_id = "1AChv-YwvDrE-nlYdT_aRSumKc571Cqxk"
-        gdown.download(id=file_id, output=str(DEMANDE_PATH), quiet=False)
+        gdown.download(id=GOOGLE_DRIVE_FILE_ID, output=str(DEMANDE_PATH), quiet=False)
     except Exception as e:
         print(f"Erreur lors du téléchargement ou de la décompression : {e}")
         exit(1)
