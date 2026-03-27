1946dc27919aac23b62cf1b3fd3bb9c52384b931
Jérôme Fréchette <jerome.frechette@polymtl.ca>
2026-03-26 22:07:32 -0400

    add guard to simulation page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { DocsPage } from './pages/docs-page/docs-page';
 import { NotFoundPage } from './pages/not-found-page/not-found-page';
 import { MapPage } from './pages/map-page/map-page';
 import { SimulationPage } from './pages/simulation-page/simulation-page';
+import { simulationGuard } from './guards/simulation.guard';
 
 export const routes: Routes = [
     { path: '', component: HomePage },
     { path: 'à-propos', component: AboutPage },
     { path: 'documentation', component: DocsPage },
     { path: 'map', component: MapPage },
-    { path: 'simulation', component: SimulationPage },
+    { path: 'simulation', component: SimulationPage, canActivate: [simulationGuard] },
     { path: '**', component: NotFoundPage },
 ];
