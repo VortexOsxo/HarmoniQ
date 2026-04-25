67801b6a8b95af354d81ca445bb2630bbbc80f94
Yacine Barka <byacine121@gmail.com>
2026-04-25 15:33:39 -0400

    fixed backend test

--- harmoniQ/tests/test_offshore_mesh.py
+++ harmoniQ/tests/test_offshore_mesh.py
@@ -18,6 +18,7 @@ from harmoniq.scripts.eolien.build_quebec_offshore_mesh import (
 
 def _session():
     engine = create_engine("sqlite:///:memory:")
+    engine = engine.execution_options(schema_translate_map={"reseau": None})
     schemas.SQLBase.metadata.create_all(bind=engine)
     Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     return Session()
